25713dd9c8a0fa1c2cff7bf8348b5eef83bd53a5
Kernytskyj Bohdan <kernytskyi_ak23@nuwm.edu.ua>
2024-11-24 17:23:52 +0200

    initial project template

--- app_blog/tests.py
+++ app_blog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import HomePageView
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func.view_class,
+                         HomePageView)
+def test_category_view_status_code(self):
+    url = reverse('articles-category-list', args=('name',))
+    response = self.client.get(url)
+    self.assertEquals(response.status_code, 200)
